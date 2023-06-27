5bb04d47ec54d3dc7e689023bc7726c65d0b5bdb
Vladimir <tolstov@gmail.com>
2023-06-27 14:44:42 +0700

    Удален вывод интро

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -6,11 +6,6 @@
         <div class="content-header">
             <h1>{{$title}}</h1>
         </div>
-        @isset($introtext)
-            <div class="content-intro">
-                <p>{{$introtext}}</p>
-            </div>
-        @endisset
     </div>
     <div class="content">
         {!! $content !!}
