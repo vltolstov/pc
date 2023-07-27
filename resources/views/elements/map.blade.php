68aefd79a69398eafa7def0614486a2ab4b2cae4
Vladimir <tolstov@gmail.com>
2023-07-27 13:58:12 +0700

    Сделан вывод проектов

--- resources/views/elements/map.blade.php
+++ resources/views/elements/map.blade.php
@@ -10,27 +10,47 @@
                     <object class="map-svg" data="/images/map.svg" type="image/svg+xml"></object>
                 </div>
                 <div class="map-flex">
-                    <a href="#" class="map-project" data-id="1" data-city="bratsk" style="background-image: url('/images/default-800x600.png')">
-                        <p>Текстовое поле для вывода длиного заголовка с переносом строки множеством букв</p>
-                    </a>
-                    <a href="#" class="map-project" data-id="2" data-city="moskva" style="background-image: url('/images/default-800x600.png')">
-                        <p>Запуск плавильной печи GWJ 0.45-300-1</p>
-                    </a>
-                    <a href="#" class="map-project" data-id="3" data-city="novosibirsk" style="background-image: url('/images/default-800x600.png')">
-                        <p>Поставка дробеметной установки проходного типа QB4948</p>
-                    </a>
-                    <a href="#" class="map-project" data-id="4" data-city="perm" style="background-image: url('/images/default-800x600.png')">
-                        <p>Текстовое поле для вывода заголовка</p>
-                    </a>
-                    <a href="#" class="map-project" data-id="5" data-city="penza" style="background-image: url('/images/default-800x600.png')">
-                        <p>Текстовое поле для вывода заголовка</p>
-                    </a>
-                    <a href="#" class="map-project" data-id="6" data-city="tver" style="background-image: url('/images/default-800x600.png')">
-                        <p>Текстовое поле для вывода заголовка</p>
-                    </a>
-                    <a href="#" class="map-project" data-id="7" data-city="vladivostok" style="background-image: url('/images/default-800x600.png')">
-                        <p>Текстовое поле для вывода заголовка</p>
-                    </a>
+                    @foreach($projects as $project)
+                        @if(isset($project->image))
+                            <a href="{{$project->urn}}"
+                                class="map-project"
+                                data-id="{{$loop->iteration}}"
+                                @foreach(json_decode($project->params, true) as $param)
+                                    @if($param['name'] == 'город')
+                                        data-city="{{Str::slug($param['value'], '-')}}"
+                                    @endif
+                                @endforeach
+                                style="background-image: url('{{json_decode($project->image, true)['image-1']['200x150']}}')"
+                            >
+                                <p>{{$project->name}}</p>
+                            </a>
+                        @else
+                            <img src="/images/default-200x150.png" alt=" ">
+                        @endif
+
+                    @endforeach
+
+{{--                    <a href="#" class="map-project" data-id="1" data-city="bratsk" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Текстовое поле для вывода длиного заголовка с переносом строки множеством букв</p>--}}
+{{--                    </a>--}}
+{{--                    <a href="#" class="map-project" data-id="2" data-city="moskva" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Запуск плавильной печи GWJ 0.45-300-1</p>--}}
+{{--                    </a>--}}
+{{--                    <a href="#" class="map-project" data-id="3" data-city="novosibirsk" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Поставка дробеметной установки проходного типа QB4948</p>--}}
+{{--                    </a>--}}
+{{--                    <a href="#" class="map-project" data-id="4" data-city="perm" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Текстовое поле для вывода заголовка</p>--}}
+{{--                    </a>--}}
+{{--                    <a href="#" class="map-project" data-id="5" data-city="penza" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Текстовое поле для вывода заголовка</p>--}}
+{{--                    </a>--}}
+{{--                    <a href="#" class="map-project" data-id="6" data-city="tver" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Текстовое поле для вывода заголовка</p>--}}
+{{--                    </a>--}}
+{{--                    <a href="#" class="map-project" data-id="7" data-city="vladivostok" style="background-image: url('/images/default-800x600.png')">--}}
+{{--                        <p>Текстовое поле для вывода заголовка</p>--}}
+{{--                    </a>--}}
                 </div>
             </div>
         </div>
