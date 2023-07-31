299d6f08118b992694752a5e8c9e669ec252dda6
Vladimir <tolstov@gmail.com>
2023-07-31 14:59:18 +0700

    Оптимизирован показ баннеров

--- resources/views/elements/slider.blade.php
+++ resources/views/elements/slider.blade.php
@@ -1,6 +1,6 @@
 <div class="slider">
     @foreach($slides as $slide)
-        <div class="slider-wrap" style="background-image: url('{{json_decode($slide->images, true)['image-1']['2000x1500']}}')">
+        <div class="slider-wrap" style="background-image: url('{{json_decode($slide->images, true)['image-1']['1200x900']}}')">
             <div class="container">
                     @switch(json_decode($slide->params, true)[0]['value'])
                         @case('top-left')
@@ -16,12 +16,17 @@
                             <div class="slide-bottom-right">
                         @break
                     @endswitch
-                                <div class="slide-flex-item">
-                                    <p class="slide-header">{{$slide->title}}</p>
-                                    <div class="slide-info">{!! $slide->content !!}</div>
-                                </div>
-                            </div>
+                    <div class="slide-flex-item">
+                        <p class="slide-header">{{$slide->title}}</p>
+                        <div class="slide-info">{!! $slide->content !!}</div>
+                    </div>
+                </div>
             </div>
         </div>
+    @endforeach
+</div>
+<div style="position:fixed;left:9999px;opacity:0;">
+    @foreach($slides as $slide)
+        <img src="{{json_decode($slide->images, true)['image-1']['1200x900']}}">
     @endforeach
 </div>
