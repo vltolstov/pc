dda3cc56349c58480a3fcdb9ec2cd6362eabbb62
Vladimir <tolstov@gmail.com>
2023-08-01 12:13:06 +0700

    Отключен скрол на карте

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -29,7 +29,7 @@
         </div>
         <div class="col-lg-6">
             <div class="footer-map">
-                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9fe861dc8205928f4318a2dcfa1f4c077fe13708d5a3322ec2609a8cbcc3f58e&amp;width=968&amp;height=653&amp;lang=ru_RU&amp;scroll=true"></script>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9fe861dc8205928f4318a2dcfa1f4c077fe13708d5a3322ec2609a8cbcc3f58e&amp;width=968&amp;height=653&amp;lang=ru_RU&amp;scroll=false"></script>
             </div>
         </div>
     </div>
